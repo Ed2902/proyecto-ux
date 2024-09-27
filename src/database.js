ab97ba41c12177f02e3e64e1a0a8da249e134a78
Ed2902 <ebernal@uniempresarial.edu.co>
2024-09-26 21:59:36 -0500

    docker finalizado

--- src/database.js
+++ src/database.js
@@ -1,11 +1,11 @@
-import {createPool} from 'mysql2/promise';
+import { createPool } from 'mysql2/promise';
 
 const pool = createPool({
-    host:'localhost',
-    port: '3306',
-    user: 'root',
-    password: '',
-    database: 'viatge_bd'
+  host: 'localhost',  // Docker está corriendo MySQL en localhost
+  port: '3306',       // El puerto que mapeamos es 3306
+  user: 'usuario',    // El usuario que definimos en docker-compose.yml
+  password: 'contrasena',  // La contraseña que definimos en docker-compose.yml
+  database: 'viatge_bd'  // Nombre de la base de datos que configuraste
 });
 
 export default pool;
